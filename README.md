465d0eec8cac16d5f6b34c59d7a1558eeefb5994
morino-kumasan <74145127+morino-kumasan@users.noreply.github.com>
2022-08-09 14:07:00 +0900

    暗号化時にも保存先を指定するよう変更

--- README.md
+++ README.md
@@ -15,11 +15,11 @@ build
 encrypt
 
 ```
-./enc.sh /path/to/folder
+./enc.sh /path/to/source/folder /path/to/destination/folder
 ```
 
 decrypt
 
 ```
-./dec.sh ./dest/20220101_000000.dat /path/to/folder
+./dec.sh ./dest/20220101_000000.dat /path/to/destination/folder
 ```
--- enc.sh
+++ enc.sh
@@ -3,6 +3,7 @@
 export MSYS_NO_PATHCONV=1
 
 target="$(realpath "$1")"
+dest="$(realpath "$2")"
 
 now=$(date --utc +"%Y%m%d_%H%M%S")
-docker run --rm -it -v "${target}:/src:ro" -v "$(pwd)/dest:/dest" backup enc /src "/dest/${now}.dat"
+docker run --rm -it -v "${target}:/src:ro" -v "${dest}:/dest" backup enc /src "/dest/${now}.dat"
